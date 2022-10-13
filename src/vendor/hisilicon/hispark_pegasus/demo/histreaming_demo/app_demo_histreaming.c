5acff6149a1ff223f46721ae9cf144c8030e846c
唯教授 <2530308275@qq.com>
2022-10-13 14:33:09 +0800

    modify histreaming_demo warning

--- src/vendor/hisilicon/hispark_pegasus/demo/histreaming_demo/app_demo_histreaming.c
+++ src/vendor/hisilicon/hispark_pegasus/demo/histreaming_demo/app_demo_histreaming.c
@@ -27,12 +27,12 @@
 #include <hi_io.h>
 #include <hi_task.h>
 #include <hi_types_base.h>
-#include "app_demo_multi_sample.h"
 #include "iot_gpio.h"
 #include "ssd1306_oled.h"
 #include "app_demo_i2c_oled.h"
 #include "hi_stdlib.h"
 #include "hi_mem.h"
+#include "app_demo_multi_sample.h"
 #include <histreaming.h>
 
 #define HISTREAMING_TASK
